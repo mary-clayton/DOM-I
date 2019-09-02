02d0ae35436c2df1d83f701efe31dfe9314451a9
Mary <mary.clayton1@hotmail.com>
2019-09-02 11:25:52 -0700

    added more code to index.js file

--- js/index.js
+++ js/index.js
@@ -6,6 +6,7 @@ const siteContent = {
     "nav-item-4": "Features",
     "nav-item-5": "About",
     "nav-item-6": "Contact",
+    "nav-item-7": "Location",
     "img-src": "img/logo.png"
   },
   "cta": {
@@ -42,17 +43,27 @@ let logo = document.querySelector("#logo-img").setAttribute('src', siteContent["
 
 let one = document.getElementsByTagName("a")[0];
 one.textContent = (siteContent["nav"]["nav-item-1"]);
+one.style.color = "green";
 let two = document.getElementsByTagName("a")[1];
 two.textContent = (siteContent["nav"]["nav-item-2"]);
+two.style.color = "green";
 let three = document.getElementsByTagName("a")[2];
 three.textContent = (siteContent["nav"]["nav-item-3"]);
+three.style.color = "green";
 let four = document.getElementsByTagName("a")[3];
 four.textContent = (siteContent["nav"]["nav-item-4"]);
+four.style.color = "green";
 let five = document.getElementsByTagName("a")[4];
 five.textContent = (siteContent["nav"]["nav-item-5"]);
+five.style.color = "green";
 let six = document.getElementsByTagName("a")[5];
 six.textContent = (siteContent["nav"]["nav-item-6"]);
-
+six.style.color = "green";
+let seventh = document.createElement("a");
+document.querySelector("nav").appendChild(seventh);
+seventh.href = "#";
+let seven = document.getElementsByTagName("a")[6].textContent = (siteContent["nav"]["nav-item-7"]);
+seventh.style.color = "green";
 
 let header = document.querySelector(".cta h1").textContent = (siteContent["cta"]["h1"]);
 let headerImage = document.querySelector("#cta-img").setAttribute('src', siteContent["cta"]["img-src"]);
@@ -60,34 +71,36 @@ let button = document.querySelector("button").textContent = (siteContent["cta"][
 
 let middleImg =document.querySelector(".middle-img").setAttribute('src', siteContent ["main-content"]["middle-img-src"]);
 
-let featuresHeader = document.querySelector(".top-content h4");
-featuresHeader.textContent = (siteContent["main-content"]["features-h4"]);
-let featuresPara = document.querySelector(".top-content p");
-featuresPara.textContent = (siteContent["main-content"]["features-content"]);
+let parent = document.getElementsByClassName("top-content");
+let child = document.querySelectorAll(".top-content h4");
+child[0].textContent = (siteContent["main-content"]["features-h4"]);
+child[1].textContent = (siteContent["main-content"]["about-h4"]);
 
-let aboutHeader = document.querySelector(".top-content h4");
-aboutHeader.textContent = (siteContent["main-content"]["about-h4"]);
-let aboutPara = document.querySelector(".top-content p");
-aboutPara.textContent = (siteContent["main-content"]["about-content"]);
+let child2 =document.querySelectorAll(".top-content p");
+child2[0].textContent = (siteContent["main-content"]["features-content"]);
+child2[1].textContent = (siteContent["main-content"]["about-content"]);
+
+let parent2 = document.getElementsByClassName("bottom-content");
+let child3 =document.querySelectorAll(".bottom-content h4");
+child3[0].textContent = (siteContent["main-content"]["services-h4"]);
+child3[1].textContent = (siteContent["main-content"]["product-h4"]);
+child3[2].textContent = (siteContent["main-content"]["vision-h4"]);
+
+let child4 =document.querySelectorAll(".bottom-content p");
+child4[0].textContent = (siteContent["main-content"]["services-content"]);
+child4[1].textContent = (siteContent["main-content"]["product-content"]);
+child4[2].textContent = (siteContent["main-content"]["vision-content"]);
+
+let parent3 = document.getElementsByClassName("contact");
+let child5 = document.querySelectorAll(".contact h4");
+child5[0].textContent = (siteContent["contact"]["contact-h4"]);
+
+let child6 =document.querySelectorAll(".contact p");
+child6[0].textContent = (siteContent["contact"]["address"]);
+child6[1].textContent = (siteContent["contact"]["phone"]);
+child6[2].textContent = (siteContent["contact"]["email"]);
 
 
-let servicesHeader = document.querySelector(".bottom-content h4");
-servicesHeader.textContent = (siteContent["main-content"]["services-h4"]);
-let servicesPara = document.querySelector(".bottom-content p");
-servicesPara.textContent = (siteContent["main-content"]["services-content"]);
-
-let productsHeader = document.querySelector(".bottom-content h4");
-productsHeader.textContent = (siteContent["main-content"]["product-h4"]);
-let productsPara = document.querySelector(".bottom-content p");
-productsPara.textContent = (siteContent["main-content"]["product-content"]);
-
-
-let contactHeader = document.querySelector(".contact h4");
-contactHeader.textContent = (siteContent["contact"]["contact-h4"]);
-let contactAddress = document.querySelector(".contact p").textContent = (siteContent["contact"]["address"]);
-let contactPhone = document.querySelector(".contact p").textContent = (siteContent["contact"]["phone"]);
-let contactEmail = document.querySelector(".contact p").textContent = (siteContent["contact"]["email"]);
-
-let footer = document.querySelector("footer p".textContent = siteContent["footer"]["copyright"]);
+let footer = document.querySelector("footer p").textContent = (siteContent["footer"]["copyright"]);
 
 
